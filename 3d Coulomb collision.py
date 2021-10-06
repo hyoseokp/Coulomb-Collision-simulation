98d5b0a1ca4743352a9f68ede97646be59fc1090
hyoseokp <42889193+hyoseokp@users.noreply.github.com>
2021-10-06 22:50:01 +0900

    Update 3d Coulomb collision.py

--- 3d Coulomb collision.py
+++ 3d Coulomb collision.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.integrate import odeint
+from matplotlib.animation import FuncAnimation
 
 class Coulomb_Collsion:
 
@@ -78,37 +79,63 @@ class Coulomb_Collsion:
     def plot_trajectory(self):
         
         x1, y1, z1, x2, y2, z2, vec = self.Collision()
-        plt.figure()
+        
+        p1 = np.sqrt(vec[self.t*self.time_resolution-1,3]**2 + 
+                     vec[self.t*self.time_resolution-1,4]**2 +
+                     vec[self.t*self.time_resolution-1,5]**2
+                    )
+        
         theta1 = np.arctan(y1[self.t*self.time_resolution-1]/x1[self.t*self.time_resolution-1])*180/np.pi
         theta2 = -np.arctan(y2[self.t*self.time_resolution-1]/x2[self.t*self.time_resolution-1])*180/np.pi
+        
+        plt.figure()
         plt.plot(x1, y1, '-',label='scattering angle='+'%.2f'%(theta1))
         plt.plot(x2, y2, '-',label='rebound angle='+'%.2f'%(theta2))
         plt.axis('equal')
         plt.title('Trajectory')
         plt.xlabel('x')
         plt.ylabel('y')
-#         plt.ylim([-0.25,0.25])
-#         plt.xlim([-0.25,0.25])
+        plt.ylim([-0.25,0.25])
+        plt.xlim([-0.25,0.25])
         plt.show()
-
+        
         print('scattering angle :',theta1)
         print('rebound angle :',theta2)
         print('angle sum:',theta1+theta2)
-        p1 = np.sqrt(vec[self.t*self.time_resolution-1,2]**2 + vec[self.t*self.time_resolution-1,3]**2)
         print('total momentum :' , p1)
         
         return 0
-    
+    def ani(self):
+        x1, y1, z1, x2, y2, z2, vec = self.Collision()
+        
+        X1, Y1 = [], []
+        X2, Y2 = [], []
+        line1, = plt.plot([], [], '-')
+        line2, = plt.plot([], [], '-')
+        
+        def update(frame):
+            X1.append(x1[frame])
+            Y1.append(y1[frame])
+            X2.append(x2[frame])
+            Y2.append(y2[frame])
+            line1.set_data(X1, Y1)
+            line2.set_data(X2, Y2)
+        return line,
+
+
+        ani = FuncAnimation(fig, update, frames=np.linspace(0, self.t, self.t*self.time_resolution))
+        plt.show()
+
 c = Coulomb_Collsion()
 
-m1    = 1   ;     m2    = 100
-q1    = 1   ;     q2    = -1
-x1_0  = -1 ;      y1_0  = 1     ;  z1_0  = 0
-x2_0  = 0   ;     y2_0  = -1    ;  z2_0  = 0
-vx1_0 = 0.1  ;    vy1_0 = 0     ;  vz1_0 = 0
-vx2_0 = 0  ;      vy2_0 = 0     ;  vz2_0 = 0
+[m1, q2]
+[m2, q1]
+[x1_0, y1_0, z1_0]    = -10, 0.01, 0
+[x2_0, y2_0, z2_0]    = 0, -0.01, 0
+[vx1_0, vy1_0, vz1_0] = 10, 0, 0
+[vx2_0, vy2_0, vz2_0] = 0, 0, 0
 k = 1
-t = 5
+t = 3
 time_resolution = 1000
 
 c.set_init_condition(m1, m2, q1, q2, 
@@ -118,3 +145,4 @@ c.set_init_condition(m1, m2, q1, q2,
                      vx2_0, vy2_0, vz2_0, 
                      k, t, time_resolution)
 c.plot_trajectory()
+c.ani()
